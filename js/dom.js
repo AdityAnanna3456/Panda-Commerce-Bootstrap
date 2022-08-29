7b98f789ff4204feb31e1507cc0dd0313f5db6bd
AdityAnanna <adityananna14@gmail.com>
2022-08-29 20:45:04 +0600

    final commit and finished with responsiveness

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,8 @@
 const element = document.getElementsByClassName("panda-btn-buy-now");
 element.addEventListener("click", myfunction())
 function myfunction (){
-	document.getElementById("subscribe").innerHTML = "Hello World";
-	innerHTML=""
+	console.log('buy now btn clicked');
+	
 }
 
 
