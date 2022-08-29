68d9c2d312a9b7ab81fda8b2fd773f1cb67ca9fa
AdityAnanna <adityananna14@gmail.com>
2022-08-29 20:41:14 +0600

    last commit and javascript corrected

--- js/dom.js
+++ js/dom.js
@@ -2,6 +2,7 @@ const element = document.getElementsByClassName("panda-btn-buy-now");
 element.addEventListener("click", myfunction())
 function myfunction (){
 	document.getElementById("subscribe").innerHTML = "Hello World";
+	innerHTML=""
 }
 
 
