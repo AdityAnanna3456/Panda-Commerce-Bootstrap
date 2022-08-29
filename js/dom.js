1021c8e415ae25202c714a7b4a495470087b96c3
AdityAnanna <adityananna14@gmail.com>
2022-08-29 20:39:29 +0600

    added js file

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+const element = document.getElementsByClassName("panda-btn-buy-now");
+element.addEventListener("click", myfunction())
+function myfunction (){
+	document.getElementById("subscribe").innerHTML = "Hello World";
+}
+
+
